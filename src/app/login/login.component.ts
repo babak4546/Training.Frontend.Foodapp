a5e390eebbd64071efe50f42b12eee71ec2bf342
Babak <babak.ares23@gmail.com>
2023-06-14 00:26:57 +0330

    complete login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 
 import { FormControl, Validators } from '@angular/forms';
+import { LoginService } from './login.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  username = new FormControl('', [Validators.required, Validators.minLength(6),Validators.maxLength(18)]);
+  constructor(private loginService:LoginService){}
+  username = new FormControl('', [Validators.required, Validators.minLength(5),Validators.maxLength(18)]);
   password = new FormControl('', [Validators.required, Validators.minLength(8),Validators.maxLength(32)]);
-
+  login(){
+    this.loginService.login(this.username.value??'',this.password.value??'');
+  }
 }
